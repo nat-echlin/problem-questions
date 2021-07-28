c8fb07518b6e995a60f68cf913a940f19eabda37
nat <nat.echlin@gmail.com>
2021-07-28 20:55:08 +0100

    started improved lcv2

---
+++ think like a programmer book questions/LCV 2.js
@@ -0,0 +1,26 @@
+const validate = input => {
+    const listNum = input.toString().split('')
+    const total = {
+        even: 0,
+        odd: 0
+    }
+
+    listNum.forEach(element, index => {
+        digit = element.toString()
+        const toAdd = {
+            even: undefined,
+            odd: undefined
+        }
+        if (index % 2 == 0) { // index is even
+            // double, 
+            // need to treat double as seperate digits?
+            // if yes, 
+
+
+            
+            // dont double, increment odd 
+        } else {
+            // dont double, increment odd 
+        }
+    })
+}
