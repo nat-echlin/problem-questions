5d046b29845618c5dd8a9926548a3f0f24387f99
nat <nat.echlin@gmail.com>
2021-07-30 22:01:51 +0100

    completed check fn, progress on sort fn

--- think like a programmer book questions/Sorting an Array.js
+++ think like a programmer book questions/Sorting an Array.js
@@ -1,5 +1,26 @@
 const arr = ["abscs", "", "aoun,sjs7", "bawcnvda", "as", "dasje", ";msosnf", "asjkg da wah", ", asdaw  sda"]
 
-console.log(
-    "asdkw".length
-)
+const test = [1, 2, 3]
+const test2 = ["abc", "ab", "a"]
+
+const check = array => {
+    return array.every((element, index) => {
+        if (index == array.length - 1 || element.length >= array[index + 1].length) {
+            return [true]
+        } else {
+            return [false, index, index + 1]
+        }
+    })    
+}
+
+const sort = array => {
+    fArray = array
+    do {
+        result = check(fArray)
+        if (result[0] == false) {
+            fArray.insert()
+
+        }
+    } while (!result[0])
+    return fArray
+}
