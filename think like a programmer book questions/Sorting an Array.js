f7720e6351841854bcf59b4ae56d51d918f1a0ae
nat <nat.echlin@gmail.com>
2021-07-31 18:13:01 +0100

    pseudocode complete

--- think like a programmer book questions/Sorting an Array.js
+++ think like a programmer book questions/Sorting an Array.js
@@ -19,8 +19,19 @@ const sort = array => {
         result = check(fArray)
         if (result[0] == false) {
             fArray.insert()
-
         }
     } while (!result[0])
     return fArray
 }
+
+// [1, 2, 3, 4]
+// // insert [2] at [1]
+// [1, 3, 2, 3, 4]
+// // pop [3]
+// [1, 3, 2, 4]
+
+
+// const x = [1, 2, 3, 4]
+// x.splice(1, 0, x[2])
+// x.splice(3, 1)
+// console.log(x)
