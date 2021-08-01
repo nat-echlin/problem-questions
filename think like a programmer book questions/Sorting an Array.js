a89bfb6180f4027741edf45f5e411e0f24b82f20
nat <nat.echlin@gmail.com>
2021-08-01 10:30:13 +0100

    fixed check fn returns

--- think like a programmer book questions/Sorting an Array.js
+++ think like a programmer book questions/Sorting an Array.js
@@ -1,16 +1,29 @@
 const arr = ["abscs", "", "aoun,sjs7", "bawcnvda", "as", "dasje", ";msosnf", "asjkg da wah", ", asdaw  sda"]
 
-const test = [1, 2, 3]
-const test2 = ["abc", "ab", "a"]
+const testCases = [
+    [1, 2, 3],
+    ["abc", "ab", "a"],
+    ["a", "ab", "a"]
+]
 
 const check = array => {
-    return array.every((element, index) => {
+    isValid = array.every((element, index) => {
         if (index == array.length - 1 || element.length >= array[index + 1].length) {
-            return [true]
+            return true
         } else {
-            return [false, index]
+            return false
         }
-    })    
+    })
+    if (isValid) {
+        return [true]
+    } else {
+        return [
+            false,
+            array.findIndex((element, index) => {
+                index == array.length - 1 || element.length >= array[index + 1].length
+            })
+        ]
+    }
 }
 
 // doesnt return as array, only returns as true / false
@@ -39,5 +52,5 @@ const sort = array => {
 // x.splice(1, 0, x[2])
 // x.splice(3, 1)
 // console.log(x)
-
+console.log(!undefined)
 console.log(sort(test2))
