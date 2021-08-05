fb82eeabdf57c34f7f876b9b88b338caac65b174
nat <nat.echlin@gmail.com>
2021-08-05 13:41:38 +0100

    worked on parrot recursion, copying from book :(

--- think like a programmer book questions/Parrot Recursion.js
+++ think like a programmer book questions/Parrot Recursion.js
@@ -1,11 +1,23 @@
-const parrotNumbers = [7, 5, 3, 10, 2]
+const parrotNumbers = [7, 5, 3, 10, 2] // in the book, the amount of parrots at each station
 
-const nonRecursiveMethod = () => {
+const nonRecursiveMethod = (arr, size) => {
     let sum = 0
-    for(let i = 0; i < parrotNumbers.length; i++) {
-        sum += parrotNumbers[i]
+    for(let i = 0; i < size; i++) {
+        sum += arr[i]
     }
     return sum
 }
 
-console.log(nonRecursiveMethod())
+console.log(nonRecursiveMethod(parrotNumbers, parrotNumbers.length))
+// returns 27, correct
+
+const arraySumDelegate = (arr, size) => {
+    const size = arr.length
+    if (size == 0) {
+        return 0
+    } 
+
+    const lastNumber = arr[size - 1]
+    const allButLastNum = nonRecursiveMethod(arr, size - 1)
+    return lastNumber + allButLastNum
+}
